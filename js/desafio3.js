5630424b28adb9aa82ee88b22dd2c73fec1abf73
Martin Gigena Curchod <magc13@hotmail.com>
2021-11-01 20:17:41 -0300

    desafio subido

---
+++ js/desafio3.js
@@ -0,0 +1,7 @@
+let tabla = Number(prompt("Que tabla queres saber???"));
+
+for (let i = 1; i<=10; i++){
+    let resultado = tabla * i;
+    alert (tabla + " X " + i + " = " + resultado);
+    console.log(tabla + " X " + i + " = " + resultado);
+}
